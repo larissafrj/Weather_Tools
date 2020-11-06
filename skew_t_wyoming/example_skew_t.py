dc8812829434dda0bc71c851e643444609ba2a99
Larissa de Freitas <42750686+larissafrj@users.noreply.github.com>
2020-11-06 17:18:02 -0300

    Adding a title

--- skew_t_wyoming/example_skew_t.py
+++ skew_t_wyoming/example_skew_t.py
@@ -3,6 +3,8 @@ import datetime
 from skewT_functions import skewT_functions
 skewT_functions = skewT_functions()
 
+"""example_skew_t.py: This code has an example of application to the functions implemented in skewT_functions.py."""
+
 date = datetime.datetime(2020,8,6,0)
 location_id = 83840
 df_sounding, df_instability_index = skewT_functions.extract_soundings(date, location_id)
