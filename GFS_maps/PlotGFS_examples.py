be41b2a1b253926c346095beaae268197121c303
larissafrj <42750686+larissafrj@users.noreply.github.com>
2021-01-09 20:20:53 -0300

    minor fixes

--- GFS_maps/PlotGFS_examples.py
+++ GFS_maps/PlotGFS_examples.py
@@ -15,6 +15,7 @@ from PlotGFS_functions import PlotGFS_functions
 """PlotGFS_example.py: This code has an example of application 
 to the functions implemented in PlotGFS_functions.py."""
 
+#File from https://www.ncei.noaa.gov/thredds/catalog/model-gfs-g4-anl-files/catalog.html
 dset = xr.open_dataset('gfs_4_20200701_0000_000.grb2',engine='pynio')
 dset = dset.sel(lat_0=slice(15,-60), lon_0=slice(360-108, 350))
 
